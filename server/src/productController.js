5245b68071568b5bd7965116db3ba2c193e35437
StanislavVelykoivan <Veli_yo76@student.itstep.org>
2023-12-06 10:51:14 +0100

    fix

--- server/src/productController.js
+++ server/src/productController.js
@@ -1,42 +1,58 @@
 const azure = require('azure-storage');
 const multer = require('multer');
 const { BlobServiceClient } = require('@azure/storage-blob');
-
+const { QueueServiceClient } = require('@azure/storage-queue');
+const sharp = require('sharp');
 
 const azureConnectionString = 'AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;DefaultEndpointsProtocol=http;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;QueueEndpoint=http://127.0.0.1:10001/devstoreaccount1;TableEndpoint=http://127.0.0.1:10002/devstoreaccount1;';
 const tableService = azure.createTableService(azureConnectionString);
 const productsTableName = 'products';
 const containerClient = BlobServiceClient.fromConnectionString(azureConnectionString).getContainerClient('productimages');
+const queueName = 'imagequeue';
 
+const queueServiceClient = QueueServiceClient.fromConnectionString(azureConnectionString);
+const imageQueueClient = queueServiceClient.getQueueClient(queueName);
 
 const storage = multer.memoryStorage();
 const upload = multer({ storage: multer.memoryStorage() });
 
 
-const addProduct = (req, res) => {
-  const newProduct = req.body;
-  newProduct.RowKey = Date.now().toString();
-  newProduct.PartitionKey = 'default';
+const addProduct = async (req, res) => {
+  try {
+    const newProduct = req.body;
+    newProduct.RowKey = Date.now().toString();
+    newProduct.PartitionKey = 'default';
 
+    if (req.file) {
+      // Получаем имя продукта из запроса
+      const productName = req.body.name;
+      const imageBlobName = `image_${productName}.jpg`;
 
-  if (req.file) {
-    const imageBlobName = `image_${Date.now()}.jpg`;
-    const blockBlobClient = containerClient.getBlockBlobClient(imageBlobName);
-    const imageBuffer = req.file.buffer;
-    blockBlobClient.upload(imageBuffer, imageBuffer.length);
+      // Загружаем изображение в блоб
+      const blockBlobClient = containerClient.getBlockBlobClient(imageBlobName);
+      await blockBlobClient.upload(req.file.buffer, req.file.buffer.length);
 
-    newProduct.image = imageBlobName;
-  }
-  newProduct.category = req.body.category;
-  tableService.insertEntity(productsTableName, newProduct, (error, result, response) => {
-    if (error) {
-      console.error(`Error adding product: ${error}`);
-      res.status(500).send('Internal Server Error');
-    } else {
-      res.send('Product added successfully!');
+      // Отправляем только имя блоба в виде ссылки в сообщении очереди
+      await imageQueueClient.sendMessage(JSON.stringify({ blobName: imageBlobName }));
     }
-  });
+
+    // Добавляем продукт в таблицу
+    newProduct.category = req.body.category;
+    newProduct.image = `${req.body.name}.jpg`; // Имя файла = имя продукта
+    tableService.insertEntity(productsTableName, newProduct, (error, result, response) => {
+      if (error) {
+        console.error('Error adding product:', error);
+        res.status(500).send('Internal Server Error');
+      } else {
+        res.send('Product added successfully!');
+      }
+    });
+  } catch (error) {
+    console.error('Error adding product:', error);
+    res.status(500).send('Internal Server Error');
+  }
 };
+
 
 
 const getAllProductsWithImages = (req, res) => {
--- server/src/server.js
+++ server/src/server.js
@@ -34,9 +34,8 @@ app.get('/products/category/:category', productController.getProductsByCategory)
 
 app.post('/uploadImage/:imageName', upload.single('image'), async (req, res) => {
   try {
-    const imageName = req.params.imageName; 
+    const imageName = req.params.imageName;
     const imageBuffer = req.file.buffer;
-
 
     await imageQueueClient.sendMessage(JSON.stringify({ imageName, imageBuffer }));
 
@@ -47,57 +46,32 @@ app.post('/uploadImage/:imageName', upload.single('image'), async (req, res) => 
   }
 });
 
-const processImageQueue = async () => {
-  try {
-    const messages = await imageQueueClient.receiveMessages({ numberOfMessages: 1 });
-    
-    if (messages.receivedMessageItems.length > 0) {
-      const message = messages.receivedMessageItems[0];
-      const { productId, imageBuffer } = JSON.parse(message.messageText);
 
-
-      const resizedImageBuffer = await sharp(imageBuffer).resize(64, 64).toBuffer();
-
-
-      const blobName = `thumbnail_${productId}.jpg`;
-      const blockBlobClient = containerClient.getBlockBlobClient(blobName);
-      await blockBlobClient.upload(resizedImageBuffer, resizedImageBuffer.length);
-
-
-      await imageQueueClient.deleteMessage(message.messageId, message.popReceipt);
-
-      console.log(`Image for product ${productId} processed and thumbnail saved.`);
-    }
-  } catch (error) {
-    console.error(error);
-  } finally {
-
-    setTimeout(processImageQueue, 1000);
-  }
-};
-
-
-processImageQueue();
 app.get('/images/:imageName', async (req, res) => {
   const imageName = req.params.imageName;
 
   try {
-    const blockBlobClient = containerClient.getBlockBlobClient(imageName);
-    const downloadBlockBlobResponse = await blockBlobClient.download();
+    // Формируем имя конвертированного блоба
+    const convertedImageBlobName = `converted_image_${imageName}`;
+    
+    // Получаем блоб конвертированного изображения
+    const convertedBlockBlobClient = containerClient.getBlockBlobClient(convertedImageBlobName);
+    const downloadBlockBlobResponse = await convertedBlockBlobClient.download();
     const content = await streamToBuffer(downloadBlockBlobResponse.readableStreamBody);
 
+    // Устанавливаем заголовки ответа
     res.writeHead(200, {
       'Content-Type': downloadBlockBlobResponse.contentType,
       'Content-Length': content.length,
     });
 
+    // Отправляем конвертированное изображение
     res.end(content);
   } catch (error) {
-    console.error('Error fetching image:', error);
-    res.status(404).send('Image not found');
+    console.error('Error fetching converted image:', error);
+    res.status(404).send('Converted Image not found');
   }
 });
-
 
 async function streamToBuffer(readableStream) {
   return new Promise((resolve, reject) => {
